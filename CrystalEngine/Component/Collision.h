30377d33031c5619cc691268ab0af3b5223c886a
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-24 20:11:46 +0800

    Create Collision.h

---
+++ CrystalEngine/Component/Collision.h
@@ -0,0 +1,13 @@
+#ifndef COLLISHION_H
+#define COLLISHION_H
+#include "CrystalEngine/Scene/Component.h"
+namespace CrystalEngine
+{
+class Collision : public Component
+{
+public:
+    Collision();
+    ~Collision();
+};
+} // namespace CrystalEngine
+#endif
