9e677caf2c2ce1c693800d3da4938667385ba17e
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-24 14:59:33 +0800

    Update main.cpp

--- CrystalEngine/main.cpp
+++ CrystalEngine/main.cpp
@@ -15,21 +15,19 @@ public:
 		CrystalEngine::GameObject *obj2 = new CrystalEngine::GameObject("G2");
 		CrystalEngine::GameObject *obj3 = new CrystalEngine::GameObject("G3");
 
-		std::cout << "qwq";
+		Comparison(obj1->addChild(obj2), true);
+		Comparison(obj1->addChild(obj3), true);
 
-		Comparison(obj1->addChild(obj2), true)
-			Comparison(obj1->addChild(obj3), true)
+		Comparison(obj1->addChild(obj2), true);
+		Comparison(obj1->addChild(obj3), false);
 
-				Comparison(obj1->addChild(obj2), false)
-					Comparison(obj1->addChild(obj3), false)
+		Comparison(obj2->getParten(), obj1);
+		Comparison(obj3->getParten(), obj1);
 
-						Comparison(obj2->getParten(), obj1)
-							Comparison(obj3->getParten(), obj1)
+		Comparison(obj1->getChildrenCount(), 2);
+		Comparison(obj1->getChildren().size(), 2);
 
-								Comparison(obj1->getChildrenCount(), 2)
-									Comparison(obj1->getChildren().size(), 2)
-
-										delete obj1;
+		delete obj1;
 		delete obj2;
 		delete obj3;
 	}
@@ -37,10 +35,9 @@ public:
 
 int main()
 {
-	while (true)
+	do
 	{
-		new TestGameObject();
-		start();
 		dispose();
-	}
+		new TestGameObject();
+	} while (!start());
 }
