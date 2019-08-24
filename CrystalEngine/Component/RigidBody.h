97c24bfe50c70f6053ee2f00a21fe94380baf1e6
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-24 20:11:38 +0800

    Create RigidBody.h

---
+++ CrystalEngine/Component/RigidBody.h
@@ -0,0 +1,14 @@
+#ifndef RIGIDBODY_H
+#define RIGIDBODY_H
+
+#include "CrystalEngine/Scene/Component.h"
+namespace CrystalEngine
+{
+class RigidBody : public Component
+{
+public:
+    RigidBody();
+    ~RigidBody();
+};
+} // namespace
+#endif
