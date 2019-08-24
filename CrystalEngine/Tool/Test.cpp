f58f7924c21af305b947a82fd3d918dc1603d0da
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-24 14:16:14 +0800

    Update Test.cpp

--- CrystalEngine/Tool/Test.cpp
+++ CrystalEngine/Tool/Test.cpp
@@ -2,37 +2,17 @@
 
 namespace CrystalEngine
 {
-void Test::start()
-{
-    for (Test *var : *Test::instance)
-    {
-        var->run();
-    }
-    std::cout << "Test the function to stop running." << std::endl
-              << "The number of errors is " << error_count << std::endl;
-}
-void Test::dispose()
-{
-    // while (!Test::instance->empty())
-    // {
-    //     delete Test::instance->back();
-    //     Test::instance->pop_back();
-    // }
-    for (size_t i = 0; i < Test::instance->size(); i++)
-    {
-        delete (*Test::instance)[i];
-    }
-}
 
 Test::Test(std::string _name)
-{
+{   
     name = _name;
-    for (Test *var : instance)
+    for (Test *var : *instance)
     {
         if (var->name == _name)
             throw this;
     }
-    instance.push_back(this);
+    instance->push_back(this);
+    std::cout<<instance->size();
 }
 Test::~Test()
 {
