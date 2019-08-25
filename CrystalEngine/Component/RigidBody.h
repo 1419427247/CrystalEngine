baf8b93b0cc0f48afb7e6ca654da52ecbe09e3f3
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-25 09:41:32 +0800

    Update RigidBody.h

--- CrystalEngine/Component/RigidBody.h
+++ CrystalEngine/Component/RigidBody.h
@@ -1,14 +1,36 @@
 #ifndef RIGIDBODY_H
 #define RIGIDBODY_H
-
+#include"CrystalEngine/Component/Vector.h"
 #include "CrystalEngine/Scene/Component.h"
 namespace CrystalEngine
 {
+enum BodyType
+{
+	staticBody,
+	kinematicBody,
+	dynamicBody,
+};
+
 class RigidBody : public Component
 {
+	Vector linearVelocity;
+	Vector angularVelocity;
+
+	double linearDamping;
+	double angularDamping;
+    double gravityScale;
+
+	bool allowSleep;
+	bool awake;
+	bool fixedRotation;
+	bool bullet;
+	bool type;
+	bool active;
 public:
     RigidBody();
     ~RigidBody();
+    
+    void update();
 };
 } // namespace
 #endif
