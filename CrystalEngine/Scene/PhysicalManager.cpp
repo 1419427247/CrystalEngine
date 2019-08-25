720e953cb081d22960586dd6e9503e488595dc4e
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-25 13:21:10 +0800

    Create PhysicalManager.cpp

---
+++ CrystalEngine/Scene/PhysicalManager.cpp
@@ -0,0 +1,22 @@
+#include "CrystalEngine/Scene/PhysicalManager.h"
+
+namespace CrystalEngine
+{
+PhysicalManager::PhysicalManager(Scene* _scene)
+{
+    scene = _scene;
+}
+PhysicalManager::~PhysicalManager()
+{
+
+}
+void PhysicalManager::run()
+{
+
+}
+void PhysicalManager::finish()
+{
+
+}
+
+} // namespace CrystalEngine
