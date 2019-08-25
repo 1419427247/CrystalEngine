b5333213232c746610a61e6f695fe447713a27a6
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-25 13:20:52 +0800

    Update main.cpp

--- CrystalEngine/main.cpp
+++ CrystalEngine/main.cpp
@@ -15,26 +15,6 @@
 
 int main()
 {
-	b2Vec2 gravity(0.0f, -10.0f);
-
-	b2World world(gravity);
-
-	b2BodyDef bodyDef;
-	bodyDef.position.Set(1, 3);
-	bodyDef.type = b2BodyType::b2_dynamicBody;
-
-	b2CollidePolygons
 	
-
-	b2Body *box = world.CreateBody(&bodyDef);
-
-	world.Step(0.02, 10, 2);
-
-	std::cout << box->GetPosition().y << std::endl;
-
-	world.Step(0.02, 10, 2);
-
-	std::cout << box->GetPosition().y << std::endl;
-	std::cout << bodyDef.position.y << std::endl;
 	return 0;
 }
