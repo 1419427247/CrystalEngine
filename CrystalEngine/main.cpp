3fd2b94a0c4c2398fc9fc92bcfea0d265c3adbfb
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-28 21:28:32 +0800

    Update main.cpp

--- CrystalEngine/main.cpp
+++ CrystalEngine/main.cpp
@@ -14,5 +14,6 @@ int main()
 	// 	testDispose();
 	// 	new CrystalEngine::TestScene();
 	// } while (!testBegin());
-	
+	CrystalEngine::Date date= CrystalEngine::Time::now();
+	std::cout << date.year <<date.month << date.day << date.hour << date.minute << date.second;
 }
