a8d0b18aa5b0b2ee0976af6d422ab86320bd06a8
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-28 21:28:29 +0800

    Update Time.cpp

--- CrystalEngine/Tool/Time.cpp
+++ CrystalEngine/Tool/Time.cpp
@@ -1,10 +1,26 @@
-
+#include <thread>
+#include <ctime>
+#include <chrono>
 
 #include "CrystalEngine/Tool/Time.h"
 #include "CrystalEngine/Tool/Runnable.h"
 
 namespace CrystalEngine
 {
+
+
+Date Time::now(){
+    time_t _t = time(NULL);
+    tm* _tm = localtime(&_t);
+    return Date{
+        _tm->tm_year + 1900,
+        _tm->tm_mon + 1,
+        _tm->tm_mday,
+        _tm->tm_hour,
+        _tm->tm_min,
+        _tm->tm_sec,
+    };
+}
 
 void Time::run(Runnable *_runable, int _millisecond)
 {
