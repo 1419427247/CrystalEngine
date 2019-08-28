1d1d2bfce5b81b35d6402ad1708de6f242434b10
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-28 20:57:13 +0800

    Update main.cpp

--- CrystalEngine/main.cpp
+++ CrystalEngine/main.cpp
@@ -1,10 +1,18 @@
 #include "CrystalEngine/Application.h"
 #include "Box2D/Box2D.h"
+
+class a{
+	public:
+	bool print(){
+		std::cout<<"qwq";
+	}
+};
 int main()
 {
-	do
-	{
-		testDispose();
-		new CrystalEngine::TestScene();
-	} while (!testBegin());
+	// do
+	// {
+	// 	testDispose();
+	// 	new CrystalEngine::TestScene();
+	// } while (!testBegin());
+	
 }
