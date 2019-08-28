948521871b19c76274db1706144e8e8d3cbf031c
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-28 20:57:02 +0800

    Create Runnable.cpp

---
+++ CrystalEngine/Tool/Runnable.cpp
@@ -0,0 +1,16 @@
+#include "CrystalEngine/Tool/Runnable.h"
+
+namespace CrystalEngine
+{
+Runnable::Runnable()
+{
+    alive = false;
+}
+Runnable::~Runnable()
+{
+
+}
+void Runnable::run(){
+
+}
+} // namespace CrystalEngine
