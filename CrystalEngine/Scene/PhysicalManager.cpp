6ad7cab9e57bdb817f55cc6f99718697e9626e22
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-29 19:28:52 +0800

    Update PhysicalManager.cpp

--- CrystalEngine/Scene/PhysicalManager.cpp
+++ CrystalEngine/Scene/PhysicalManager.cpp
@@ -3,36 +3,37 @@
 
 namespace CrystalEngine
 {
-PhysicalManager::PhysicalManager(Scene *_scene)
+PhysicalManager::PhysicalManager()
 {
-    scene = _scene;
 }
 PhysicalManager::~PhysicalManager()
-{
-}
-void PhysicalManager::run()
-{
-}
-void PhysicalManager::finish()
 {
 }
 
 void PhysicalManager::newRigidBody(RigidBody *_rigidBody)
 {
-    
 }
 void PhysicalManager::destoryRigidBody(RigidBody *_rigidBody)
 {
-
 }
 
 void PhysicalManager::newCollision(Collision *_collision)
 {
-
 }
 void PhysicalManager::destoryCollision(Collision *_collision)
 {
+}
 
+void PhysicalManager::start()
+{
+}
+
+void PhysicalManager::update()
+{
+}
+
+void PhysicalManager::destory()
+{
 }
 
 } // namespace CrystalEngine
