5a85baabcd609b65a116282031b61f52770dca69
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-29 17:00:54 +0800

    Update main.cpp

--- CrystalEngine/main.cpp
+++ CrystalEngine/main.cpp
@@ -1,12 +1,40 @@
 #include "CrystalEngine/Application.h"
 #include "Box2D/Box2D.h"
 
-class a{
-	public:
-	bool print(){
-		std::cout<<"qwq";
+class a : public CrystalEngine::Runnable
+{
+public:
+	int i = 0;
+	a()
+	{
+	}
+	void run()
+	{
+		if (i++ < 10)
+			std::cout << "qwq";
+		else
+			exit();
 	}
 };
+
+
+class Temp : public CrystalEngine::Component{
+	public:
+	Temp():Component("Temp"){
+
+	}
+	void start(){
+
+	}
+	void update()
+	{
+		
+	}
+	void destory(){
+
+	}
+};
+
 int main()
 {
 	// do
@@ -14,4 +42,11 @@ int main()
 	// 	testDispose();
 	// 	new CrystalEngine::TestScene();
 	// } while (!testBegin());
+	//CrystalEngine::Scene* scene = new CrystalEngine::Scene();
+	CrystalEngine::Runnable *r = new a();
+	CrystalEngine::Time t;
+	t.startLoop(r, 1000);
+
+	while (r->isAlive())
+		;
 }
