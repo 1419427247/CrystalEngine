2f1d77545a74941483410b6aa614fd77d582aa76
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-29 19:28:16 +0800

    Update Timer.h

--- CrystalEngine/Tool/Timer.h
+++ CrystalEngine/Tool/Timer.h
@@ -1,12 +1,16 @@
 #ifndef TIME_H
 #define TIME_H
 
+#include <thread>
+#include <ctime>
+#include <chrono>
 #include <functional>
+
 #include "CrystalEngine/Tool/Test.h"
 
 namespace CrystalEngine
 {
-	
+
 struct Date
 {
 	int year;
@@ -32,11 +36,49 @@ public:
 
 	static void run(std::function<bool()> _runable, int _millisecond);
 	static void start(std::function<bool()> _runable, int _millisecond);
-	
+
 	static void runLoop(std::function<bool()> _runable, int _millisecond);
 	static void startLoop(std::function<bool()> _runable, int _millisecond);
 
+	template <class T>
+	static void run(T *_runable, bool (T::*_fun)(), int _millisecond)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(_millisecond));
+		(_runable->*_fun)();
+	}
+
+	template <class T>
+	static void start(T *_runable, bool (T::*_fun)(), int _millisecond)
+	{
+		std::thread thread = std::thread([_runable, _fun, _millisecond]() {
+			std::this_thread::sleep_for(std::chrono::milliseconds(_millisecond));
+			(_runable->*_fun)();
+		});
+		thread.detach();
+	}
+
+	template <class T>
+	static void runLoop(T *_runable, bool (T::*_fun)(), int _millisecond)
+	{
+		while ((_runable->*_fun)())
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(_millisecond));
+		}
+	}
+	template <class T>
+	static void startLoop(T *_runable, bool (T::*_fun)(), int _millisecond)
+	{
+		std::thread thread = std::thread([_runable, _fun, _millisecond]() {
+			while ((_runable->*_fun)())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(_millisecond));
+			}
+		});
+		thread.detach();
+	}
+
 	static void sleep(int _millisecond);
+	static void yield();
 };
 } // namespace CrystalEngine
 #endif
