d7b46250801870e2d323837fb82b2f7a909485e8
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-29 19:29:10 +0800

    Update Application.cpp

--- CrystalEngine/Application.cpp
+++ CrystalEngine/Application.cpp
@@ -1,11 +1,22 @@
 ﻿#include "CrystalEngine/Application.h"
+
 namespace CrystalEngine
 {
-Application::Application()
+Application::Application(Scene *_scene)
 {
+    scene = _scene;
 }
 
 Application::~Application()
 {
+}
+void Application::run()
+{
+    scene->start();
+    // while (scene->update())
+    // {
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    // }
+    //Timer::runLoop<Scene>(scene->update,20);
 }
 } // namespace CrystalEngine
