5982d865d941c08b245354105eef49d24046b7c1
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-08-29 17:00:31 +0800

    Create Timer.h

---
+++ CrystalEngine/Tool/Timer.h
@@ -0,0 +1,35 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include "CrystalEngine/Tool/Test.h"
+
+namespace CrystalEngine
+{
+	
+struct Date
+{
+	int year;
+	int month;
+	int day;
+	int hour;
+	int minute;
+	int second;
+};
+
+class Runnable;
+class Timer
+{
+private:
+public:
+	static Date now();
+
+	static void run(Runnable *_runable, int _millisecond);
+	static void start(Runnable *_runable, int _millisecond);
+
+	static void runLoop(Runnable *_runable, int _millisecond);
+	static void startLoop(Runnable *_runable, int _millisecond);
+
+	static void sleep(int _millisecond);
+};
+} // namespace CrystalEngine
+#endif
