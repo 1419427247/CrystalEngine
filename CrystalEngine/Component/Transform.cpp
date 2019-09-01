52f4e5b27c0a95a1f96014e3932bc3f1d08dab6a
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-09-01 17:43:41 +0800

    Update Transform.cpp

--- CrystalEngine/Component/Transform.cpp
+++ CrystalEngine/Component/Transform.cpp
@@ -1,26 +1,22 @@
 #include "CrystalEngine/Component/Transform.h"
+#include "CrystalEngine/Component/Vector.h"
 
 namespace CrystalEngine
 {
 Transform::Transform() : Component("Transform")
 {
     position = new Vector();
-    rotate = new Vector(1,0);
+    rotate = 0;
 }
 
 Transform::Transform(Transform& _t): Component("Transform"){
     position = new Vector(*_t.position);
-    rotate = new Vector(*_t.rotate);
+    rotate = _t.rotate;
 }
 
 Transform::~Transform()
 {
     delete position;
-    delete rotate;
-}
-
-double Transform::getAngle(){
-    return atan(rotate->getY()/rotate->getX());
 }
 
 } // namespace CrystalEngine
