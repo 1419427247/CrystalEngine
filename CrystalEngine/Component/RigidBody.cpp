4ed69368fa0d1501bc150c1ca13a13937fcb075b
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-09-01 17:43:32 +0800

    Update RigidBody.cpp

--- CrystalEngine/Component/RigidBody.cpp
+++ CrystalEngine/Component/RigidBody.cpp
@@ -1,38 +1,136 @@
 #include "CrystalEngine/Scene/Scene.h"
 #include "CrystalEngine/Scene/GameObject.h"
 #include "CrystalEngine/Scene/PhysicalManager.h"
+
 #include "CrystalEngine/Component/RigidBody.h"
+#include "CrystalEngine/Component/Vector.h"
 
 namespace CrystalEngine
 {
 RigidBody::RigidBody() : Component("RigidBody")
 {
-    linearVelocity = new Vector();
-    angularVelocity = new Vector(0,1);
-    linearDamping = 0.0f;
-    angularDamping = 0.0f;
-    allowSleep = true;
-    awake = true;
-    fixedRotation = false;
-    bullet = false;
-    type = BodyType::dynamicBody;
-    active = true;
-    gravityScale = 1.0f;
 }
 RigidBody::~RigidBody()
 {
 }
+
+// Vector* RigidBody::getPosition() const
+// {
+//     return gameObject->scene->physicalManager->getPosition(this);
+// }
+double RigidBody::getRotate()
+{
+    return gameObject->scene->physicalManager->getRotate(this);
+}
+
+// Vector* RigidBody::getLinearVelocity() const
+// {
+//     return gameObject->scene->physicalManager->getLinearVelocity(this);
+// }
+double RigidBody::getAngularVelocity()
+{
+    return gameObject->scene->physicalManager->getAngularVelocity(this);
+}
+
+double RigidBody::getLinearDamping()
+{
+    return gameObject->scene->physicalManager->getLinearDamping(this);
+}
+double RigidBody::getAngularDamping()
+{
+    return gameObject->scene->physicalManager->getAngularDamping(this);
+}
+double RigidBody::getGravityScale()
+{
+    return gameObject->scene->physicalManager->getGravityScale(this);
+}
+
+bool RigidBody::isSleepingAllowed()
+{
+    return gameObject->scene->physicalManager->isSleepingAllowed(this);
+}
+bool RigidBody::isAwake()
+{
+    return gameObject->scene->physicalManager->isAwake(this);
+}
+bool RigidBody::isFixedRotation()
+{
+    return gameObject->scene->physicalManager->isFixedRotation(this);
+}
+bool RigidBody::isBullet()
+{
+    return gameObject->scene->physicalManager->isBullet(this);
+}
+BodyType RigidBody::getType()
+{
+    return gameObject->scene->physicalManager->getType(this);
+}
+bool RigidBody::isActive()
+{
+    return gameObject->scene->physicalManager->isActive(this);
+}
+
+void RigidBody::setTransform(Vector _vector, double _angle)
+{
+    gameObject->scene->physicalManager->setTransform(this, _vector, _angle);
+}
+
+void RigidBody::setLinearVelocity(Vector _linearVelocity)
+{
+    gameObject->scene->physicalManager->setLinearVelocity(this, _linearVelocity);
+}
+void RigidBody::setAngularVelocity(double _angularVelocity)
+{
+    gameObject->scene->physicalManager->setAngularVelocity(this, _angularVelocity);
+}
+
+void RigidBody::setLinearDamping(double _linearDamping)
+{
+    gameObject->scene->physicalManager->setLinearDamping(this, _linearDamping);
+}
+void RigidBody::setAngularDamping(double _angularDamping)
+{
+    gameObject->scene->physicalManager->setAngularDamping(this, _angularDamping);
+}
+void RigidBody::setGravityScale(double _gravityScale)
+{
+    gameObject->scene->physicalManager->setGravityScale(this, _gravityScale);
+}
+
+void RigidBody::setSleepingAllowed(bool _allowSleep)
+{
+    gameObject->scene->physicalManager->setSleepingAllowed(this, _allowSleep);
+}
+void RigidBody::setAwake(bool _awake)
+{
+    gameObject->scene->physicalManager->setAwake(this, _awake);
+}
+void RigidBody::setFixedRotation(float _fixedRotation)
+{
+    gameObject->scene->physicalManager->setFixedRotation(this, _fixedRotation);
+}
+void RigidBody::setBullet(bool _bullet)
+{
+    gameObject->scene->physicalManager->setBullet(this, _bullet);
+}
+void RigidBody::setType(BodyType _bodyType)
+{
+    gameObject->scene->physicalManager->setType(this, _bodyType);
+}
+void RigidBody::setActive(bool _active)
+{
+    gameObject->scene->physicalManager->setActive(this, _active);
+}
+
 void RigidBody::start()
 {
-    if (gameObject->scene->physicalManager)
-        gameObject->scene->physicalManager->newRigidBody(this);
+    gameObject->scene->physicalManager->newRigidBody(this);
 }
 void RigidBody::update()
 {
 }
 void RigidBody::destory()
 {
-    if (gameObject->scene->physicalManager)
-        gameObject->scene->physicalManager->destoryRigidBody(this);
+    gameObject->scene->physicalManager->destoryRigidBody(this);
 }
 } // namespace CrystalEngine
