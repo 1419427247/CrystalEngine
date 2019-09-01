714b653e0e4da31a5887bb8e5ff65da8b94d4f1b
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-09-01 17:44:15 +0800

    Update Box2dPhysical.h

--- CrystalEngine/Tool/Box2dPhysical.h
+++ CrystalEngine/Tool/Box2dPhysical.h
@@ -2,7 +2,10 @@
 #define BOX2DPHYSICAL_H
 #include "CrystalEngine/Scene/PhysicalManager.h"
 
+#include <unordered_map>
+
 class b2World;
+class b2Body;
 namespace CrystalEngine
 {
 class GameObject;
@@ -11,6 +14,8 @@ class RigidBody;
 class Collision;
 class Box2dPhysical : public PhysicalManager
 {
+    std::unordered_map<std::string,b2Body*>* bodies;
+
     b2World* world;
     Vector* gravity;
 public:
@@ -19,10 +24,35 @@ public:
     void newRigidBody(RigidBody *_rigidBody);
     void destoryRigidBody(RigidBody *_rigidBody);
 
+   // Vector* getPosition(const RigidBody *_rigidBody)const;
+	double getRotate(RigidBody *_rigidBody);
+	//Vector* getLinearVelocity(const RigidBody *_rigidBody)const;
+	double getAngularVelocity(RigidBody *_rigidBody);
+	double getLinearDamping(RigidBody *_rigidBody);
+	double getAngularDamping(RigidBody *_rigidBody);
+	double getGravityScale(RigidBody *_rigidBody);
+	bool isSleepingAllowed(RigidBody *_rigidBody);
+	bool isAwake(RigidBody *_rigidBody);
+	bool isFixedRotation(RigidBody *_rigidBody);
+	bool isBullet(RigidBody *_rigidBody);
+	BodyType getType(RigidBody *_rigidBody);
+	bool isActive(RigidBody *_rigidBody);
+
+    void setTransform(RigidBody *_rigidBody, Vector _vector,double _angle);
+	void setLinearVelocity(RigidBody *_rigidBody, Vector _linearVelocity);
+	void setAngularVelocity(RigidBody *_rigidBody, double _angularVelocity);
+	void setLinearDamping(RigidBody *_rigidBody, double _linearDamping);
+	void setAngularDamping(RigidBody *_rigidBody, double _angularDamping);
+	void setGravityScale(RigidBody *_rigidBody, double _gravityScale);
+	void setSleepingAllowed(RigidBody *_rigidBody, bool _allowSleep);
+	void setAwake(RigidBody *_rigidBody, bool _awake);
+	void setFixedRotation(RigidBody *_rigidBody, float _fixedRotation);
+	void setBullet(RigidBody *_rigidBody, bool _bullet);
+	void setType(RigidBody *_rigidBody, BodyType _bodyType);
+	void setActive(RigidBody *_rigidBody, bool _active);
+
     void newCollision(Collision *_collision);
     void destoryCollision(Collision *_collision);
-
-    
 
     void start();
 
