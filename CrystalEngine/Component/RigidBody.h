c701824c5cb83d454bf70a6b8ea722b9dc79bc27
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-09-01 17:43:35 +0800

    Update RigidBody.h

--- CrystalEngine/Component/RigidBody.h
+++ CrystalEngine/Component/RigidBody.h
@@ -1,10 +1,12 @@
 #ifndef RIGIDBODY_H
 #define RIGIDBODY_H
-#include"CrystalEngine/Component/Vector.h"
+
 #include "CrystalEngine/Scene/Component.h"
 namespace CrystalEngine
 {
-enum BodyType
+
+class Vector;
+enum class BodyType
 {
 	staticBody,
 	kinematicBody,
@@ -14,30 +16,49 @@ enum BodyType
 class RigidBody : public Component
 {
 public:
-	Vector* linearVelocity;
-	Vector* angularVelocity;
+	RigidBody();
+	~RigidBody();
 
-	double linearDamping;
-	double angularDamping;
-    double gravityScale;
+	void* getBodyData();
 
-	bool allowSleep;
-	bool awake;
-	bool fixedRotation;
-	bool bullet;
-	BodyType type;
-	bool active;
+	//Vector* getPosition() const;
+	double getRotate();
 
-    RigidBody();
-    ~RigidBody();
-    
-	void setPosition();
-	void setRotate();
+	// Vector* getLinearVelocity() const;
+	double getAngularVelocity();
+
+	double getLinearDamping();
+	double getAngularDamping();
+	double getGravityScale();
+
+	bool isSleepingAllowed();
+	bool isAwake();
+	bool isFixedRotation();
+	bool isBullet();
+	BodyType getType();
+	bool isActive();
+
+	void setBodyData(void* _bodyData);
+
+    void setTransform(Vector _vector,double _angle);
+
+	void setLinearVelocity(Vector _linearVelocity);
+	void setAngularVelocity(double _angularVelocity);
+
+	void setLinearDamping(double _linearDamping);
+	void setAngularDamping(double _angularDamping);
+	void setGravityScale(double _gravityScale);
+
+	void setSleepingAllowed(bool _allowSleep);
+	void setAwake(bool _awake);
+	void setFixedRotation(float _fixedRotation);
+	void setBullet(bool _bullet);
+	void setType(BodyType _bodyType);
+	void setActive(bool _active);
 
 	void start() override;
-    void update() override;
+	void update() override;
 	void destory() override;
-
 };
-} // namespace
+} // namespace CrystalEngine
 #endif
