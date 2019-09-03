c34fcce31f2778fa5055825f384d74f758068c6e
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-09-03 17:24:12 +0800

    Update PhysicalManager.cpp

--- CrystalEngine/Scene/PhysicalManager.cpp
+++ CrystalEngine/Scene/PhysicalManager.cpp
@@ -1,5 +1,8 @@
 #include "CrystalEngine/Tool/Vector.h"
+
 #include "CrystalEngine/Component/RigidBody.h"
+#include "CrystalEngine/Component/Transform.h"
+
 #include "CrystalEngine/Scene/GameObject.h"
 #include "CrystalEngine/Scene/PhysicalManager.h"
 
@@ -15,35 +18,102 @@ PhysicalManager::~PhysicalManager()
 void PhysicalManager::newRigidBody(RigidBody *_rigidBody) {}
 void PhysicalManager::destoryRigidBody(RigidBody *_rigidBody) {}
 
-Vector PhysicalManager::getPosition(RigidBody *_rigidBody) {}
-double PhysicalManager::getRotate(RigidBody *_rigidBody) {}
-Vector PhysicalManager::getLinearVelocity(RigidBody *_rigidBody) {}
-double PhysicalManager::getAngularVelocity(RigidBody *_rigidBody) {}
-double PhysicalManager::getLinearDamping(RigidBody *_rigidBody) {}
-double PhysicalManager::getAngularDamping(RigidBody *_rigidBody) {}
-double PhysicalManager::getGravityScale(RigidBody *_rigidBody) {}
-bool PhysicalManager::isSleepingAllowed(RigidBody *_rigidBody) {}
-bool PhysicalManager::isAwake(RigidBody *_rigidBody) {}
-bool PhysicalManager::isFixedRotation(RigidBody *_rigidBody) {}
-bool PhysicalManager::isBullet(RigidBody *_rigidBody) {}
-BodyType PhysicalManager::getType(RigidBody *_rigidBody) {}
-bool PhysicalManager::isActive(RigidBody *_rigidBody) {}
+Vector PhysicalManager::getPosition(RigidBody *_rigidBody)
+{
+    return *_rigidBody->gameObject->transform->position;
+}
+double PhysicalManager::getRotate(RigidBody *_rigidBody)
+{
+    return _rigidBody->gameObject->transform->rotate;
+}
+Vector PhysicalManager::getLinearVelocity(RigidBody *_rigidBody)
+{
+    throw;
+}
+double PhysicalManager::getAngularVelocity(RigidBody *_rigidBody)
+{
+    throw;
+}
+double PhysicalManager::getLinearDamping(RigidBody *_rigidBody)
+{
+    throw;
+}
+double PhysicalManager::getAngularDamping(RigidBody *_rigidBody)
+{
+    throw;
+}
+double PhysicalManager::getGravityScale(RigidBody *_rigidBody)
+{
+    throw;
+}
+bool PhysicalManager::isSleepingAllowed(RigidBody *_rigidBody)
+{
+    throw;
+}
+bool PhysicalManager::isAwake(RigidBody *_rigidBody)
+{
+    throw;
+}
+bool PhysicalManager::isFixedRotation(RigidBody *_rigidBody)
+{
+    throw;
+}
+bool PhysicalManager::isBullet(RigidBody *_rigidBody)
+{
+    throw;
+}
+BodyType PhysicalManager::getType(RigidBody *_rigidBody)
+{
+    throw;
+}
+bool PhysicalManager::isActive(RigidBody *_rigidBody)
+{
+    throw;
+}
 
-void PhysicalManager::setTransform(RigidBody *_rigidBody, Vector _vector, double _angle) {}
-void PhysicalManager::setLinearVelocity(RigidBody *_rigidBody, Vector _linearVelocity) {}
-void PhysicalManager::setAngularVelocity(RigidBody *_rigidBody, double _angularVelocity) {}
-void PhysicalManager::setLinearDamping(RigidBody *_rigidBody, double _linearDamping) {}
-void PhysicalManager::setAngularDamping(RigidBody *_rigidBody, double _angularDamping) {}
-void PhysicalManager::setGravityScale(RigidBody *_rigidBody, double _gravityScale) {}
-void PhysicalManager::setSleepingAllowed(RigidBody *_rigidBody, bool _allowSleep) {}
-void PhysicalManager::setAwake(RigidBody *_rigidBody, bool _awake) {}
-void PhysicalManager::setFixedRotation(RigidBody *_rigidBody, float _fixedRotation) {}
-void PhysicalManager::setBullet(RigidBody *_rigidBody, bool _bullet) {}
-void PhysicalManager::setType(RigidBody *_rigidBody, BodyType _bodyType) {}
-void PhysicalManager::setActive(RigidBody *_rigidBody, bool _active) {}
+void PhysicalManager::setTransform(RigidBody *_rigidBody, Vector _vector, double _angle) {
 
-void PhysicalManager::newCollision(Collision *_collision) {}
-void PhysicalManager::destoryCollision(Collision *_collision) {}
+}
+void PhysicalManager::setLinearVelocity(RigidBody *_rigidBody, Vector _linearVelocity) {
+
+}
+void PhysicalManager::setAngularVelocity(RigidBody *_rigidBody, double _angularVelocity) {
+
+}
+void PhysicalManager::setLinearDamping(RigidBody *_rigidBody, double _linearDamping) {
+
+}
+void PhysicalManager::setAngularDamping(RigidBody *_rigidBody, double _angularDamping) {
+
+}
+void PhysicalManager::setGravityScale(RigidBody *_rigidBody, double _gravityScale) {
+
+}
+void PhysicalManager::setSleepingAllowed(RigidBody *_rigidBody, bool _allowSleep) {
+
+}
+void PhysicalManager::setAwake(RigidBody *_rigidBody, bool _awake) {
+
+}
+void PhysicalManager::setFixedRotation(RigidBody *_rigidBody, float _fixedRotation) {
+
+}
+void PhysicalManager::setBullet(RigidBody *_rigidBody, bool _bullet) {
+
+}
+void PhysicalManager::setType(RigidBody *_rigidBody, BodyType _bodyType) {
+
+}
+void PhysicalManager::setActive(RigidBody *_rigidBody, bool _active) {
+    
+}
+
+void PhysicalManager::newCollision(Collision *_collision) {
+
+}
+void PhysicalManager::destoryCollision(Collision *_collision) {
+
+}
 
 void PhysicalManager::start() {}
 
