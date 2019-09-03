d53bb46208cf524bc02582cd5a435b86bac949d6
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-09-03 21:30:39 +0800

    Update Object.h

--- CrystalEngine/Tool/Object.h
+++ CrystalEngine/Tool/Object.h
@@ -4,61 +4,46 @@
 #include <unordered_map>
 #include <string>
 
-#define REGISTER(OBJECT) objects[#OBJECT] = OBJECT::instantiation
+#define REGISTER(OBJECT)                                 \
+    {                                                    \
+        using namespace CrystalEngine;                   \
+        Object::__objects[#OBJECT] = OBJECT::instantiation;\
+    }
 
-#define INSTANTIATION(OBJECT_NAME) objects[#OBJECT_NAME]()
+#define INSTANTIATION(OBJECT_NAME) new OBJECT_NAME()
 
-#define OBJECT(TYPE)                 \
-public:                              \
-    static Object *instantiation()   \
-    {                                \
-        return (Object *)new TYPE(); \
-    }                                \
-    virtual std::string getClassName()       \
-    {                                \
-        return #TYPE;          \
+#define OBJECT(OBJECT_NAME)                 \
+public:                                     \
+    static Object *instantiation()          \
+    {                                       \
+        return (Object *)new OBJECT_NAME(); \
+    }                                       \
+    virtual std::string getClassName()      \
+    {                                       \
+        return #OBJECT_NAME;                \
     }
 
 namespace CrystalEngine
 {
-
-class Object;
-static std::unordered_map<std::string, Object *(*)()> objects;
-
 class Object
 {
 public:
+    static std::unordered_map<std::string, Object *(*)()> __objects;
+
     OBJECT(Object)
 
-    Object()
-    {
+    Object();
 
-    }
-    virtual ~Object()
-    {
-    }
+    virtual ~Object();
 
-    template<class T>
+    static Object *instantiation(std::string _string);
+    virtual std::string toString();
+
+    template <class T>
     static T *instantiation(std::string _string)
     {
-        return (T*)objects[_string]();
-    }
-
-
-    static Object *instantiation(std::string _string)
-    {
-        return objects[_string]();
-    }
-
-    virtual std::string toString()
-    {
-        #ifdef OBJECT
-        return getClassName();
-        #else
-        throw();
-        #endif
+        return (T *)__objects[_string]();
     }
 };
-
 } // namespace CrystalEngine
 #endif
