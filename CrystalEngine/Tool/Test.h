fa7f0217c4a7a1a5d0ad1e27ba1ab0491cc68d83
iPad水晶 <33598732+1419427247@users.noreply.github.com>
2019-09-03 21:30:51 +0800

    Update Test.h

--- CrystalEngine/Tool/Test.h
+++ CrystalEngine/Tool/Test.h
@@ -6,6 +6,16 @@
 
 #include "CrystalEngine/Tool/Object.h"
 
+#define Comparison(T1, T2)                                                                                                 \
+    {                                                                                                                      \
+        auto var = T1;                                                                                                     \
+        if (var != T2)                                                                                                     \
+        {                                                                                                                  \
+            std::cout << "The result is wrong : " << #T1 << " == " << var << " And " << #T1 << " != " << #T2 << std::endl; \
+            error_count++;                                                                                                 \
+        }                                                                                                                  \
+    }
+
 namespace CrystalEngine
 {
 class Test : public Object
@@ -17,23 +27,14 @@ public:
     Test(std::string _name);
     virtual ~Test();
     virtual void run();
+
+    static int error_count;
+    static std::vector<CrystalEngine::Test *> instance;
+
+    static int testBegin();
+    static void testDispose();
+
 };
 } // namespace CrystalEngine
-
-extern int error_count;
-extern std::vector<CrystalEngine::Test *> *instance;
-
-extern int testBegin();
-extern void testDispose();
-
-#define Comparison(T1, T2)                                                                                                 \
-    {                                                                                                                      \
-        auto var = T1;                                                                                                     \
-        if (var != T2)                                                                                                     \
-        {                                                                                                                  \
-            std::cout << "The result is wrong : " << #T1 << " == " << var << " And " << #T1 << " != " << #T2 << std::endl; \
-            error_count++;                                                                                                 \
-        }                                                                                                                  \
-    }
 
 #endif
